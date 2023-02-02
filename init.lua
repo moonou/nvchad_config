30f3a37c79810ac16be9bff0a4740412a33f0f8b
Moonou Long <moonoulong@gmail.com>
2023-02-02 18:46:39 +0800

    feat: add focus auto reload file content

--- init.lua
+++ init.lua
@@ -3,3 +3,11 @@ vim.o.relativenumber = true
 -- 打开折叠
 vim.o.foldmethod = 'indent'
 vim.o.foldenable = false
+
+-- auto-reload files when modified externally
+-- https://unix.stackexchange.com/a/383044
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
